676fac1b55c33d745f7283130ff6afb2aff5d5fa
Steven Yuan <yuasteve@amazon.com>
2023-11-16 14:32:19 -0800

    feat(experimentalIdentityAndAuth): update `AddAwsRuntimeConfig`
    Update `AddAwsRuntimeConfig` to not add the following unless
    `@aws.api#service` is applied:
    
    - `awsCheckVersion()`
    - `region` config field and extension configuration

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddAwsRuntimeConfig.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddAwsRuntimeConfig.java
@@ -94,11 +94,13 @@ public final class AddAwsRuntimeConfig implements TypeScriptIntegration {
             writer.writeDocs("Enables FIPS compatible endpoints.")
                     .write("useFipsEndpoint?: boolean | __Provider<boolean>;\n");
         }
-        if (isSigV4Service(settings, model)) {
-            writer.writeDocs(isAwsService(settings, model)
-                                ? "The AWS region to which this client will send requests"
-                                : "The AWS region to use as signing region for AWS Auth")
-                    .write("region?: string | __Provider<string>;\n");
+        if (!settings.getExperimentalIdentityAndAuth()) {
+            if (isSigV4Service(settings, model)) {
+                writer.writeDocs(isAwsService(settings, model)
+                                    ? "The AWS region to which this client will send requests"
+                                    : "The AWS region to use as signing region for AWS Auth")
+                        .write("region?: string | __Provider<string>;\n");
+            }
         }
     }
 
